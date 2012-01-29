301d817e66aa7a91b3d965626b48e232dea68aa7
Maciej Pasternacki <maciej@pasternacki.net>
2012-01-29 19:59:45 +0100

    Bump to 0.0.7

--- lib/evoker/version.rb
+++ lib/evoker/version.rb
@@ -2,7 +2,7 @@ module Evoker
   module VERSION
     MAJOR = 0
     MINOR = 0
-    TINY  = 6
+    TINY  = 7
     STRING = [MAJOR, MINOR, TINY].join('.')
 
     def VERSION.require_version(major, minor=0, tiny=0)
