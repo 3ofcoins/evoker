5a6e1adc638f02c33b56a85317c1cb6ec37f534a
Maciej Pasternacki <maciej@pasternacki.net>
2012-12-14 17:38:34 +0100

    Typo.

--- lib/evoker/s3cache.rb
+++ lib/evoker/s3cache.rb
@@ -89,7 +89,7 @@ module Evoker
       puts " got it."
 
       tgz = Zlib::GzipReader.new(File.open(CACHE_TARBALL, 'rb'))
-      Archive::Tar::Minitar.unpack(CACHE_TARBALL, '.')
+      Archive::Tar::Minitar.unpack(tgz, '.')
     end
 
     desc "Download pre-cached entities from an S3 bucket if available; download normally and cache if not available."
